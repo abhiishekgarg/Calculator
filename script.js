fa0c9f468e0ade82c20a77b8ccbce2535829bcd4
Abhishek Garg <31281299+abhiishekgarg@users.noreply.github.com>
2020-09-25 15:45:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,3 +77,73 @@ for(var i=0; i<buttons.length; i++)
         }
     });
 }
+
+document.addEventListener("keydown", function(event) 
+{
+    var value = event.key;
+    switch(value)
+        {
+            case 'AC':
+                display.innerText = "";
+                break;
+            case '+/-':
+                operand1 = parseFloat(display.textContent);
+                operand1 *= -1;
+                display.innerText = operand1;
+                break;
+            case '%':
+                operand1 = parseFloat(display.textContent);
+                operator = '%';
+                display.innerText = operator;
+                break;
+            case '/':
+                operand1 = parseFloat(display.textContent);
+                operator = '/';
+                display.innerText = operator;
+                break;
+            case '*':
+                operand1 = parseFloat(display.textContent);
+                operator = '*';
+                display.innerText = operator;
+                break;
+            case '-':
+                operand1 = parseFloat(display.textContent);
+                operator = '-';
+                display.innerText = operator;
+                break;
+            case '+':
+                operand1 = parseFloat(display.textContent);
+                operator = '+';
+                display.innerText = operator;
+                break;
+            case '.':
+                var flag = false;
+                for(var j=0; j<display.innerText.length; j++)
+                {
+                    if(display.innerText[j] == '.')
+                    {
+                        flag = true;
+                    }
+                }
+                if(flag == false) display.innerText += ".";
+                else break;
+                break;
+            case '=':
+                operand2 = parseFloat(display.textContent);
+                if(operator == '%')
+                {
+                    operand2 /= 100;
+                    operator = '*';
+                }
+                var result = eval(operand1 + " " + operator + " " + operand2);
+                display.innerText = result;
+                break;
+            default:
+                if(display.innerText[0] == operator)
+                {
+                    display.innerText = "";
+                    
+                }
+                display.innerText += value; 
+        }
+});
